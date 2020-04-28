1c352cbdbf13e704e04d56d8d90021820e565a38
Marlon Braga <rdg.brg@gmail.com>
2020-04-28 03:26:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,2 @@
-def square_array(array)
-  counter = 0
-  new_arr = []
-  
-  while array[counter]  do
-    new_arr.push(array[counter] ** 2)
-    counter += 1
-  end
-  
-  new_arr
+def square_array 
 end
